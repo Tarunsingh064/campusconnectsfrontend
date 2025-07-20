a9302059765af60608a0cd027095ecdafe060253
tarunsingh046 <tarunworkonly@gmail.com>
2025-07-21 04:10:45 +0530

    added like and comment

--- src/components/Postfeed.jsx
+++ src/components/Postfeed.jsx
@@ -404,7 +404,7 @@ const PostCard = ({ post }) => {
                     d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" 
                   />
                 </svg>
-                <span>{post.comment_count || 0}</span>
+                {/*<span>{post.comment_count || 0}</span>*/}
               </button>
             </div>
 
